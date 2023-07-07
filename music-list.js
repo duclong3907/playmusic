9f8b16c7ac4ad55373d113b81d2d9f0137df4460
duclong3907 <duclongminhtan@gmail.com>
2023-07-07 22:30:53 +0700

    first update

---
+++ music-list.js
@@ -0,0 +1,72 @@
+// Để thêm bài nhạc mình sẽ them vào list sau
+
+//   {
+//     name: "Tên bài nhạc",
+//     artist: "Nghệ sĩ",
+//     img: "Tên ảnh định dạng .jpg trong project",
+//     src: "Link đường dẫn file nhạc đinh dạng .mp3"
+//   }
+
+let allMusic = [
+  {
+    name: "Hoa Cỏ Lau",
+    artist: "Phong, Max",
+    img: "hoa-co-lau",
+    src: "hoa-co-lau"
+  },
+ 
+  {
+    name: "Kiếp Má Hồng",
+    artist: "T.Long",
+    img: "kiep-ma-hong",
+    src: "kiep-ma-hong"
+  },
+  {
+    name: "Tự Cắt Duyên Tình",
+    artist: "Tú Na",
+    img: "tu-cat-duyen-tinh",
+    src: "tu-cat-duyen-tinh"
+  },
+  {
+    name: "Rượu Mừng Hóa Người Dưng",
+    artist: "T.Long",
+    img: "ruou-mung-hoa-nguoi-dung",
+    src: "ruou-mung-hoa-nguoi-dung"
+  },
+  {
+    name: "Pháo Hồng",
+    artist: "Đạt Vĩnh Long",
+    img: "phao-hong",
+    src: "phao-hong"
+  },
+  {
+    name: "Anh Có Đôi Bàn Tay",
+    artist: "Minh Vương M4U",
+    img: "anh-co-doi-ban-tay",
+    src: "anh-co-doi-ban-tay"
+  },
+  {
+    name: "Chàng Trai Của Em",
+    artist: "Duy Văn Phạm",
+    img: "chang-trai-cua-em",
+    src: "chang-trai-cua-em"
+  },
+  {
+    name: "Cô Đơn Trong Nhà Mình",
+    artist: "Hoài Lâm",
+    img: "co-don-trong-nha-minh",
+    src: "co-don-trong-nha-minh"
+  },
+  {
+    name: "Em Là Kẻ Đáng Thương",
+    artist: "Phát Huy",
+    img: "em-la-ke-dang-thuong",
+    src: "em-la-ke-dang-thuong"
+  },
+  {
+    name: "Anh Cứ Ngỡ",
+    artist: "Hoài Lâm",
+    img: "anh-cu-ngo",
+    src: "anh-cu-ngo"
+  },
+];
